7b89b86db85d29c40ae8ed086add968510bf4cbd
karan3267 <karanreddy3267@gmail.com>
2024-12-03 01:58:17 +0400

    Changed backend import

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Category = require("../models/category");
+const Category = require("../models/Category");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
